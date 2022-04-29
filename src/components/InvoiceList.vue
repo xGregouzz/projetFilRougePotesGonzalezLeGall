ef0fe3738a995dc32928a363612bf3f0edb9084f
Gregory Potes Gonzalez <82149103+xGregouzz@users.noreply.github.com>
2022-04-29 14:39:13 +0200

    Mise en place du @router

---
+++ src/components/InvoiceList.vue
@@ -0,0 +1,33 @@
+<template>
+    <router-view></router-view>
+    <h2>Liste des Factures : </h2>
+    <br>
+    <table>
+      <thead>
+        <tr>
+          <td>Numéro</td>
+          <td>Client</td>
+          <td>Date</td>
+          <td>Montant en €</td>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+            <td> {{numero}}</td>
+            <td></td>
+            <td></td>
+            <td></td>
+        </tr>
+      </tbody>
+    </table>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            numero: 1,
+        };
+    }
+}
+</script>
---
+++ src/views/InvoiceListView.vue
@@ -0,0 +1,12 @@
+<template>
+    <InvoiceList />
+</template>
+
+<script>
+import InvoiceList from '@/components/InvoiceList.vue'
+export default {
+    components: {
+        InvoiceList,
+    }
+}
+</script>
